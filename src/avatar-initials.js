9447255e8f26242350130c2d72de1209ac9cfa9e
nephring <nephring@gmail.com>
2019-03-04 22:00:10 +0100

    init html && js file

---
+++ src/avatar-initials.js
@@ -0,0 +1,18 @@
+/*
+  @properties
+
+  initials,
+  string,
+  size,
+  rounded,
+  cornerRadius,
+  uppercase,
+  textWeight,
+  textScale,
+  textColor,
+  textLength,
+  autoColor,
+  backgroundColor,
+  borderColor: { color, thickness }
+
+*/
