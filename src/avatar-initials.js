3f043605f8727b588c23faaf4fc9b23cdc0c408a
nephring <nephring@gmail.com>
2019-03-04 22:40:38 +0100

    init html template

--- src/avatar-initials.js
+++ src/avatar-initials.js
@@ -16,3 +16,51 @@
   borderColor: { color, thickness }
 
 */
+
+class AvatarInitials extends HTMLElement {
+  constructor() {
+    super()
+
+    this.shadow = this.attachShadow({ mode: 'open' })
+    console.log(this)
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  render() {
+    const template = document.createElement('template')
+
+    template.innerHTML = `
+      <style>
+        .avatar {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          box-sizing: border-box;
+          width: 60px;
+          height: 60px;
+          border: 1px solid white;
+          border-radius: 50%;
+          background-color: #eee;
+        }
+
+        .text {
+          font-size: 1.7em;
+          color: #1a1a1a;
+          font-family: sans-serif;
+          font-weight: bold;
+          padding: 2px;
+        }
+      </style>
+      <div class="avatar">
+        <div class="text">AB</div>
+      </div>
+  `
+
+    this.shadow.appendChild(template.content.cloneNode(true))
+  }
+}
+
+customElements.define('avatar-initials', AvatarInitials)
